73ec8fd12b3f277cc3c84756afbcfdc1a73104f2
latuvahai <133291349+latuvahai@users.noreply.github.com>
2023-05-11 20:06:46 -0500

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Article(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    content = db.Column(db.Text, nullable=False)
